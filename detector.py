7f2f4e64df0f778187b56661275539e96b3602b4
JakubSzym <kubaszym77@gmail.com>
2023-12-05 20:53:32 +0100

    made some tests

--- detector.py
+++ detector.py
@@ -4,6 +4,12 @@ import cv2
 import numpy as np 
 from matplotlib import pyplot as plt
 
+# TODO
+# 
+# zastosować wszystkie metody dopasowania i zsumować
+# usunąć nadmiarowe ramki
+#
+
 
 def performAugmentation(in_image):
     images = []
@@ -19,24 +25,33 @@ def performAugmentation(in_image):
 
     return images
 
-img_rgb = cv2.imread('parking.jpg') 
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+img_rgb = cv2.imread('board.jpg')
+img = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-template = cv2.imread('symbol.jpg',0) 
+template = cv2.imread('car.jpg')
+template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
 w, h = template.shape[::-1]
 
 templates = performAugmentation(template)
 
 locations = []
 
+#
+# metody dopasowania: TM_SQDIFF_NORMED, TM_CCOEFF_NORMED, TM_CCORR_NORMED (nieunormowane są zbyt czułe)
+# TM_SQDIFF_NORMED - najlepsze wyniki dla ufności 0.8 - 0.85
+# TM_CCOEFF_NORMED - najlepsze wyniki dla 0.4
+# TM_CCORR_NORMED - najlepsze wyniki dla 0.835
+#
+
 for t in templates:
-    res = cv2.matchTemplate(img_gray,t,cv2.TM_CCOEFF_NORMED)
-    threshold = 0.7 
+    res = cv2.matchTemplate(img, t, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.4
     loc = np.where( res >= threshold)
     locations.append(loc)
 
 for loc in locations:
-    for pt in zip(*loc[::-1]): 
+    for pt in zip(*loc[::-1]):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 3) 
 
 cv2.imwrite('items-found.jpg',img_rgb)
